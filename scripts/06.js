0400bf16fa4fdb68f5951d10a3e62d53e3bb1aba
yahor <pixaretyiypop@gmail.com>
2022-10-23 21:17:51 +0300

    50 scripts

---
+++ scripts/06.js
@@ -0,0 +1,2 @@
+var myFirstName = 'f'
+var myLastName = 's'
---
+++ scripts/30.js
@@ -0,0 +1,3 @@
+// Only change code below this line
+const myName = "Yahor";
+const myStr = "My name is" + " " + myName + " " + 'and I am well!';
